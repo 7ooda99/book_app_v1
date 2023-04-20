b993d5c2fb5db3c24eb0612e0ff04f42fda740d9
7ooda99 <mhd.alkayyal@gmail.com>
2023-04-20 20:43:28 +0300

    app done 90%

---
+++ lib/core/utils/function/custom_snak_bar.dart
@@ -0,0 +1,12 @@
+
+
+
+import 'package:flutter/material.dart';
+
+void customSnakBar(context, String text) {
+  ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+    content: Text(
+      text,
+    ),
+  ));
+}
--- lib/features/home/presentation/views/widgets/books_action.dart
+++ lib/features/home/presentation/views/widgets/books_action.dart
@@ -1,10 +1,13 @@
+import 'package:book_app/core/utils/function/launch_url.dart';
+import 'package:book_app/features/home/data/models/book_model/book_model.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../../../../core/utils/widgets/custom_button.dart';
 
 class BookAction extends StatelessWidget {
-  const BookAction({super.key});
-
+  const BookAction({super.key, required this.bookModel});
+  final BookModel bookModel;
   @override
   Widget build(BuildContext context) {
     return Padding(
@@ -16,26 +19,36 @@ class BookAction extends StatelessWidget {
               backgroundColor: Colors.white,
               textColor: Colors.black,
               borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(12),
-                bottomLeft: Radius.circular(12)
-              ), 
+                  topLeft: Radius.circular(12),
+                  bottomLeft: Radius.circular(12)),
               text: 'Free',
             ),
           ),
           Expanded(
             child: CustomButton(
+              onPressed: () async {
+                launchCustomUrl(context, bookModel.volumeInfo.previewLink!);
+              },
               fontSize: 16,
               backgroundColor: Color(0xffef8262),
               textColor: Colors.white,
-              borderRadius: BorderRadius.only(
-                bottomRight: Radius.circular(12),
-                topRight: Radius.circular(12)
-              ), 
-              text: 'Free Preview',
+              borderRadius: const BorderRadius.only(
+                  bottomRight: Radius.circular(12),
+                  topRight: Radius.circular(12)),
+              text: getText (bookModel),
             ),
           ),
         ],
       ),
     );
+  }
+  
+  String getText(BookModel bookModel) {
+    if (bookModel.volumeInfo.previewLink == null)
+    {
+      return 'Not Avaliable';
+    } else {
+      return 'Preview';
+    }
   }
 }
